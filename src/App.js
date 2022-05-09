6bf282be4cbaeac8c951f5d8842ee5f9cfdec750
Akash Tripathi <hithisisat@gmail.com>
2022-05-09 12:58:07 +0530

    added a travis cic changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          idea
+          idea 
+        </p>
+        <p>
+          idea hails
         </p>
         
       </header>
